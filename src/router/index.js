0c5d738c0ad94e04b0889851129f845be84034cb
laura <l.keeley000@gmail.com>
2022-06-12 12:19:29 -0500

    adds changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import ProductsIndex from "../views/ProductsIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -19,6 +23,10 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
+  { path: "/products", name: "products-index", component: ProductsIndex },
 ];
 
 const router = new VueRouter({
